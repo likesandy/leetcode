8670db6c357f49fe9af18951862230839a6a3e53
ilun <1445709137@qq.com>
2023-04-01 14:14:39 +0800

    ✨ feat(面试题 05.01. 插入（比较全面的一道位运算题型）):

---
+++ 程序员面试金典/面试题 05.01. 插入.ts
@@ -0,0 +1,11 @@
+// 位运算
+// 时间：O(j-i+1)
+// 空间：O1
+// 中 j-i+1 是需要清除的比特位数量，因为循环需要遍历这些比特位。因为 j-i+1 是一个常数，所以时间复杂度为 O(1)
+function insertBits(N: number, M: number, i: number, j: number): number {
+  for (let k = i; k <= j; k++) {
+    N &= ~(1 << k);
+  }
+
+  return N | (M << i);
+}
