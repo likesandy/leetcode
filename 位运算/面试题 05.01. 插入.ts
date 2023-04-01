040b1b25159c13760a6fb8431f8e9b22cfb268d9
ilun <1445709137@qq.com>
2023-04-01 17:13:34 +0800

    📃 docs(插入题添加注释（方便以后理解）):

--- 位运算/面试题 05.01. 插入.ts
+++ 位运算/面试题 05.01. 插入.ts
@@ -3,9 +3,13 @@
 // 空间：O1
 // 中 j-i+1 是需要清除的比特位数量，因为循环需要遍历这些比特位。因为 j-i+1 是一个常数，所以时间复杂度为 O(1)
 function insertBits(N: number, M: number, i: number, j: number): number {
+  // N的i~j变0
   for (let k = i; k <= j; k++) {
     N &= ~(1 << k);
   }
 
+  // 插入
   return N | (M << i);
 }
+
+insertBits(1024, 19, 2, 6);
